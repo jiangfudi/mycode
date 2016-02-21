b26218b09fc84234386a48d59eb81eceea6c0a20
chen <fsaf@fsf.com>
2016-02-21 18:27:54 +0800

    写入注释

--- app/src/main/java/com/qianfeng/zhushou/active/ui/ActiveFragment.java
+++ app/src/main/java/com/qianfeng/zhushou/active/ui/ActiveFragment.java
@@ -12,6 +12,8 @@ package com.qianfeng.zhushou.active.ui;
 //iop
 
 //jiangfudi
+
+//fjieafewa
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
